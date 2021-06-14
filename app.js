46bc4d82cbc5ed806ec1e5f1d4ffc9f7faeddf53
Priyanshu <priyanshu0627@gmail.com>
2021-06-14 20:15:35 +0530

    Implement EJS

--- app.js
+++ app.js
@@ -2,23 +2,47 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejsLint = require('ejs-lint');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
-app.get("/",function(req,res) {
+app.get("/", function(req, res) {
   var today = new Date();
-  var day = today.getDay();
+  var currDay = today.getDay();
+  var day = "";
 
-  if(day === 6 || day === 0) {
-    res.send("WeekEnd");
-  }
-  else {
-    res.send("WeekDay");
-  }
-})
+  switch (currDay) {
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    case 0:
+      day = "Sunday";
+      break;
+    default:
+      console.log("Error with day " + currDay);
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("server is running");
-})
+  }
+  res.render("lists", {
+    day: day
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
