2129c7259fdc204231fdda937b65a6e8ab04e23d
Priyanshu <priyanshu0627@gmail.com>
2021-06-16 14:21:35 +0530

    add css

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const ejsLint = require('ejs-lint');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-var items = [];
+var items = ["GFG","WebD","Back Workout","Running"];
 app.get("/", function(req, res) {
   var today = new Date();
 
@@ -16,7 +17,7 @@ app.get("/", function(req, res) {
     weekday: "long",
     day: "2-digit",
     month: "long",
-  }
+  };
   var day = today.toLocaleDateString("en-US", options);
 
   res.render("lists", {
@@ -29,7 +30,7 @@ app.post("/",function(req,res) {
   var item = req.body.item;
   items.push(item);
   res.redirect("/");
-})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
